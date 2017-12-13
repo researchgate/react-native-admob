f23984286f6b247d2716895142061be8c8e5a40a
Koen Punt <koen@koenpunt.nl>
2017-12-13 10:50:33 +0100

    perf: remove constantsToExport
    exporting constants prevents the module from being lazy loaded

--- ios/RNDFPBannerView.m
+++ ios/RNDFPBannerView.m
@@ -1,4 +1,5 @@
 #import "RNDFPBannerView.h"
+#import "RNAdMobUtils.h"
 
 #if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
@@ -103,7 +104,7 @@
 
 - (void)setValidAdSizes:(NSArray *)adSizes
 {
-    NSMutableArray *validAdSizes = [[NSMutableArray alloc] initWithCapacity:adSizes.count];
+    __block NSMutableArray *validAdSizes = [[NSMutableArray alloc] initWithCapacity:adSizes.count];
     [adSizes enumerateObjectsUsingBlock:^(id jsonValue, NSUInteger idx, __unused BOOL *stop) {
         GADAdSize adSize = [RCTConvert GADAdSize:jsonValue];
         if (GADAdSizeEqualToSize(adSize, kGADAdSizeInvalid)) {
@@ -113,6 +114,11 @@
         }
     }];
     _bannerView.validAdSizes = validAdSizes;
+}
+
+- (void)setTestDevices:(NSArray *)testDevices
+{
+    _testDevices = RNAdMobProcessTestDevices(testDevices, kDFPSimulatorID);
 }
 
 -(void)layoutSubviews
